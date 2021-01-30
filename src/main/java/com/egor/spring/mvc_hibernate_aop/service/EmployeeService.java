5542141e4b92178582b70c29b25945b75b9ffa47
Egor Anchutin <zbit16@gmail.com>
2021-01-30 20:22:29 +0300

    Add "update Employee data" button

--- src/main/java/com/egor/spring/mvc_hibernate_aop/service/EmployeeService.java
+++ src/main/java/com/egor/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -4,6 +4,8 @@ import com.egor.spring.mvc_hibernate_aop.entity.Employee;
 import java.util.List;
 
 public interface EmployeeService {
+
     public List<Employee> getAllEmployees();
     public void saveEmployee(Employee employee);
+    public Employee getEmployee(int id);
 }
